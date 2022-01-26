1042bf5c742ffe1828c28e69e8fdfb4d4ade6580
My Phạm Đỗ Hoàng <19521863@ms.uit.edu.vn>
2022-01-27 02:24:54 +0700

    Some change on app.py, offline.py,....

--- Code/app.py
+++ Code/app.py
@@ -18,26 +18,31 @@ featuresVec = pickle.load(fv)
 featuresVec = np.array(featuresVec)
 fv.close()
 
+fn = open('F:/Information Retrieval/Project/Code/featureVector/Name_Vector.py', 'rb')
+img_paths = pickle.load(fn)
+img_paths = np.array(img_paths)
+fn.close()
+
 @app.route("/", methods=["GET", "POST"])
 def page():
-    #if request.method == "POST":
-        #file = request.files["query_img"]
+    if request.method == "POST":
+        file = request.files["query_img"]
 
-    #     img = Image.open(file.stream)
-    #     uploaded_img_path = "static/uploaded/" + datetime.now().isoformat().replace(":", ".") + "_" + file.filename
-    #     img.save(uploaded_img_path)
+        img = Image.open(file.stream)
+        uploaded_img_path = "F:/Information Retrieval/Project/Code/uploaded/"+ file.filename
+        img.save(uploaded_img_path)
 
-    #     query = fe.extract(img)
-    #     dists = np.linalg.norm(feature - query, axis=1)
-    #     ids = np.argsort(dists)[:30]
-    #     scores = [(dists[id], img_paths[id]) for id in ids]
+        query = fe.get_feature(uploaded_img_path)
+        dists = np.linalg.norm(featuresVec - query, axis=1)
+        ids = np.argsort(dists)[:30]
+        scores = [(dists[id], img_paths[id]) for id in ids]
 
-    #     print(scores)
+        print(scores)
 
-    #     return render_template("page.html", query_path=uploaded_img_path, scores=scores)
-    return render_template("page.html")
-    # else:
-    #     return render_template("page.html")
+        return render_template("page.html", query_path=uploaded_img_path, scores=scores)
+
+    else:
+        return render_template("page.html")
     
     
 app.run()
